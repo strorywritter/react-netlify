ab09c578b8767296fbd5df8f1bf08f9757a3aecd
NadeeshaR <nramyashan@gmail.com>
2021-07-21 16:19:23 +0530

    update index

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React neww
+          Learn React newww
         </a>
       </header>
     </div>
